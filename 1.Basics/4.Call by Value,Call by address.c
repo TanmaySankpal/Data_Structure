1d1b910c7800fd863a9d24ef4e087499174444f6
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-11-17 21:21:07 +0530

    Add files via upload

---
+++ 1.Basics/4.Call by Value,Call by address.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node * next;
+};
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int main()
+{
+	PNODE First=NULL;
+	
+	//call by value
+	fun(First);      //fun(100)         
+	//Display(),Count()
+	
+	//call by address
+	gun(&First);    //gun(50)           
+	//InsetFirst(),InsetLast(),InsertAtPos(),DeleteFirst(),DeleteLast(),DeleteAtPos()
+	
+	return 0;
+}
